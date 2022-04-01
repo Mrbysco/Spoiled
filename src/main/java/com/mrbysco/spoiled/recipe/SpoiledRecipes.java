be8260a86b384dac3dae348890017b9ebfb9bbc3
Mrbysco <Mrbysco@users.noreply.github.com>
2022-04-01 23:48:46 +0200

    Update JEI in dev

--- src/main/java/com/mrbysco/spoiled/recipe/SpoiledRecipes.java
+++ src/main/java/com/mrbysco/spoiled/recipe/SpoiledRecipes.java
@@ -3,7 +3,6 @@ package com.mrbysco.spoiled.recipe;
 import com.mrbysco.spoiled.Reference;
 import com.mrbysco.spoiled.recipe.SpoilRecipe.SerializerSpoilRecipe;
 import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,6 +13,7 @@ public class SpoiledRecipes {
 	public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, Reference.MOD_ID);
 	public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(Registry.RECIPE_TYPE_REGISTRY, Reference.MOD_ID);
 
-	public static final RegistryObject<RecipeType<SpoilRecipe>> SPOIL_RECIPE_TYPE = RECIPE_TYPES.register("spoil_recipe", () -> new RecipeType<>() {});
+	public static final RegistryObject<RecipeType<SpoilRecipe>> SPOIL_RECIPE_TYPE = RECIPE_TYPES.register("spoil_recipe", () -> new RecipeType<>() {
+	});
 	public static final RegistryObject<SerializerSpoilRecipe> SPOILING_SERIALIZER = RECIPE_SERIALIZERS.register("spoil_recipe", SerializerSpoilRecipe::new);
 }
