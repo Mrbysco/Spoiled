b5a2283da8c395c002bc2d9e17a2dd163a2c983e
Mrbysco <Mrbysco@users.noreply.github.com>
2024-09-04 21:58:01 +0200

    Update recipe builder

--- common/src/main/java/com/mrbysco/spoiled/datagen/SpoilRecipeBuilder.java
+++ common/src/main/java/com/mrbysco/spoiled/datagen/SpoilRecipeBuilder.java
@@ -1,28 +1,24 @@
 package com.mrbysco.spoiled.datagen;
 
 import com.mrbysco.spoiled.recipe.SpoilRecipe;
-import com.mrbysco.spoiled.registration.SpoiledRecipes;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.ItemLike;
 
 public class SpoilRecipeBuilder {
 	private final Item result;
 	private final Ingredient ingredient;
 	private int spoilTime;
+	private int priority = 1;
 	private String group;
-	private final RecipeSerializer<SpoilRecipe> recipeSerializer;
 
-	private SpoilRecipeBuilder(ItemLike resultIn, Ingredient ingredientIn, RecipeSerializer<SpoilRecipe> serializer) {
+	private SpoilRecipeBuilder(ItemLike resultIn, Ingredient ingredientIn) {
 		this.result = resultIn.asItem();
 		this.ingredient = ingredientIn;
-		this.recipeSerializer = serializer;
-		this.spoilTime = -1;
 	}
 
 	private SpoilRecipeBuilder withSpoilTime(int spoilTime) {
@@ -35,8 +31,13 @@ public class SpoilRecipeBuilder {
 		return this;
 	}
 
+	private SpoilRecipeBuilder withPriority(int priority) {
+		this.priority = priority;
+		return this;
+	}
+
 	public static SpoilRecipeBuilder spoilRecipe(Ingredient ingredientIn, ItemLike resultIn) {
-		return new SpoilRecipeBuilder(resultIn, ingredientIn, SpoiledRecipes.SPOILING_SERIALIZER.get());
+		return new SpoilRecipeBuilder(resultIn, ingredientIn);
 	}
 
 	public void build(RecipeOutput recipeOutput) {
@@ -44,7 +45,7 @@ public class SpoilRecipeBuilder {
 	}
 
 	public void build(RecipeOutput recipeOutput, ResourceLocation id) {
-		SpoilRecipe recipe = new SpoilRecipe(this.group == null ? "" : this.group, this.ingredient, new ItemStack(this.result), this.spoilTime);
+		SpoilRecipe recipe = new SpoilRecipe(this.group == null ? "" : this.group, this.ingredient, new ItemStack(this.result), this.spoilTime, this.priority);
 		recipeOutput.accept(id, recipe, null);
 	}
 }
