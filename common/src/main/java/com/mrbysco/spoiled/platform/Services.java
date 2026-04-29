e82f9a1b5a67f765a2e7bf4321963b438b9971f7
Mrbysco <Mrbysco@users.noreply.github.com>
2026-04-29 15:27:25 +0200

    Fix service loader

--- common/src/main/java/com/mrbysco/spoiled/platform/Services.java
+++ common/src/main/java/com/mrbysco/spoiled/platform/Services.java
@@ -10,7 +10,7 @@ public class Services {
 	public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
 
 	public static <T> T load(Class<T> clazz) {
-		final T loadedService = ServiceLoader.load(clazz)
+		final T loadedService = ServiceLoader.load(clazz, Services.class.getClassLoader())
 				.findFirst()
 				.orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
 		Constants.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
